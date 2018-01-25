5a9efdf476a3c8f4904e71fec7ec4c7e17bbb500
Jessica Bensch <jessica.bensch11@gmail.com>
2018-01-24 23:02:59 -0500

    picture controller and views

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,23 @@
 class PicturesController < ApplicationController
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @recipe = Recipe.find(params[:id])
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      redirect_to recipe_path(@recipe)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:url, :caption)
+  end
+
 end
