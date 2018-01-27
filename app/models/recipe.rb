ee4c2cffdcbb85c895a809f96b4056a0ed9cdb5d
Jessica Bensch <jessica.bensch11@gmail.com>
2018-01-27 05:23:34 -0500

    clean up models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,7 +7,6 @@ class Recipe < ApplicationRecord
 
   def ingredients_attributes=(ingredients_attributes)
     ingredients_attributes.values.each do |ingredient_attribute|
-
       ingredient = Ingredient.find_or_create_by(ingredient_attribute)
       if !ingredient.name.blank? && !self.ingredients.include?(ingredient)
         self.ingredients << ingredient
